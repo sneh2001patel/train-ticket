040295d4edcd6ea68a331e78ab84b0f85f26b90f
17yuv <17yuvraj.sehgal@gmail.com>
2026-02-28 22:53:47 -0500

    improving ts-verification-service 2

--- ts-verification-code-service/src/main/java/verifycode/VerifyCodeApplication.java
+++ ts-verification-code-service/src/main/java/verifycode/VerifyCodeApplication.java
@@ -24,8 +24,8 @@ public class VerifyCodeApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
-                .setConnectTimeout(Duration.ofSeconds(5))
-                .setReadTimeout(Duration.ofSeconds(15))
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .setReadTimeout(Duration.ofSeconds(5))
                 .build();
     }
 }
